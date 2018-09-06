2a6d8d5efdb2177d004f562dbf08f3f2835f1b73
tal <Talevi100@gmail.com>
2018-09-06 12:57:55 +0200

    minor changes, added instafeed component, etc

---
+++ src/components/Instafeed.js
@@ -0,0 +1,16 @@
+/**
+ * Created by spacecadet on 06.09.18.
+ */
+import React, {Component} from 'react'
+
+class Instafeed extends Component {
+    render(){
+        return (
+            <div>
+                our instafeed!
+            </div>
+        )
+    }
+
+}
+export default Instafeed
